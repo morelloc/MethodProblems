b651b788ed192ab8378a9c97a63b9de857f91c24
morelloc <morellocm@gmail.com>
2013-10-21 08:43:50 -0400

    Finished Furniture, work on ZipCode

---
+++ Furniture/CheckZips/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CheckZips
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Input;
+
+            int[] ZipCode = new int[10];
+            ZipCode[0] = 01234;
+            ZipCode[1] = 12345;
+            ZipCode[2] = 23456;
+            ZipCode[3] = 34567;
+            ZipCode[4] = 45678;
+            ZipCode[5] = 56789;
+            ZipCode[6] = 67890;
+            ZipCode[7] = 78901;
+            ZipCode[8] = 89012;
+            ZipCode[9] = 90123;
+
+            Console.WriteLine("Enter your zipcode to see if we are able to deliver to your area");
+            Input = Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
---
+++ Furniture/Furniture/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Input;
+
+            Console.WriteLine("Type 'P' for Pine, 'O' for Oak, or 'M' for Mahogany");
+            Input = Console.ReadLine();
+            
+            if (Input == "P")
+            {
+                Console.WriteLine("A Pine table costs $100");
+            }
+            else if (Input == "O")
+            {
+                Console.WriteLine("An Oak table costs $225");
+            }
+            else if (Input == "M")
+            {
+                Console.WriteLine("A Mahogany table costs $310");
+            }
+            else
+            {
+                Console.WriteLine("No Price");
+            }
+        }
+    }
+}
