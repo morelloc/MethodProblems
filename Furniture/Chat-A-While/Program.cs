e6185eb96344403d19eeede039a6e03c853263fb
morelloc <morellocm@gmail.com>
2013-10-22 08:38:00 -0400

    Finished Zips (Kinda), work on Chat

---
+++ Furniture/Chat-A-While/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chat_A_While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] AreaCode = new int[6] {262, 414, 608, 715, 815, 920};
+            double[] Rate = new double[6] {0.07, 0.10, 0.05, 0.16, 0.24, 0.14};
+            double Price;
+            string Minutes, Area;
+
+            Console.WriteLine("Input area code");
+            Area = Console.ReadLine();
+
+            Console.WriteLine("Input length of call in minutes");
+            Minutes = Console.ReadLine();
+
+            
+            
+
+
+
+        }
+    }
+}
--- Furniture/CheckZips/Program.cs
+++ Furniture/CheckZips/Program.cs
@@ -9,22 +9,37 @@ namespace CheckZips
     {
         static void Main(string[] args)
         {
-            String Input;
+            string Input;
 
-            int[] ZipCode = new int[10];
-            ZipCode[0] = 01234;
-            ZipCode[1] = 12345;
-            ZipCode[2] = 23456;
-            ZipCode[3] = 34567;
-            ZipCode[4] = 45678;
-            ZipCode[5] = 56789;
-            ZipCode[6] = 67890;
-            ZipCode[7] = 78901;
-            ZipCode[8] = 89012;
-            ZipCode[9] = 90123;
+            Console.WriteLine("Enter your zipcode to see if we are able to deliver to your area.");
 
-            Console.WriteLine("Enter your zipcode to see if we are able to deliver to your area");
+            string[] ZipCode = new string[10];
+            ZipCode[0] = "01234";
+            ZipCode[1] = "12345";
+            ZipCode[2] = "23456";
+            ZipCode[3] = "34567";
+            ZipCode[4] = "45678";
+            ZipCode[5] = "56789";
+            ZipCode[6] = "67890";
+            ZipCode[7] = "78901";
+            ZipCode[8] = "89012";
+            ZipCode[9] = "90123";
+
             Input = Console.ReadLine();
+
+            for (int i = 0; i < 100; i++)
+            {
+                if (Input == ZipCode[i])
+                {
+                    Console.WriteLine("We can deliver to your area.");
+                }
+                else
+                {
+                    Console.WriteLine("We cannot deliver to your area.");
+                }
+                break;
+            }
+
 
 
 
