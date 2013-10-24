6dbec2af0620faaa59831017725027525f791dca
morelloc <morellocm@gmail.com>
2013-10-24 08:36:53 -0400

    More Twerk

---
+++ Furniture/Exponent/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Exponent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Input;
+            int Number;
+
+            Console.WriteLine("Enter a number.");
+            Input = Console.ReadLine();
+            Number = Convert.ToInt32(Input);
+
+            Squared(Number);
+            Cubed(Number);
+            Console.ReadKey();
+        }
+
+        public static void Squared(int Number)
+        {
+            int Square = Number * Number;
+            Console.WriteLine(Square);
+        }
+       
+        public static void Cubed(int Number)
+        {
+            int Cube = Number * Number * Number;
+            Console.WriteLine(Cube);
+        }
+    }
+}
--- Furniture/Monogram/Program.cs
+++ Furniture/Monogram/Program.cs
@@ -9,7 +9,14 @@ namespace Monogram
     {
         static void Main(string[] args)
         {
-            
+            DisplayMonogram("C", "J", "M");
+            DisplayMonogram("A", "J", "M");
+            Console.ReadKey();
+        }
+
+        public static void DisplayMonogram(string First, string Middle, string Last)
+        {
+            Console.WriteLine("** {0} {1} {2} **", First, Middle, Last);
         }
     }
 }
