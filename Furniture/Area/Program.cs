21ffb5b37ce949528dd7ea31a7153d83edf252e8
morelloc <morellocm@gmail.com>
2013-10-25 08:42:43 -0400

    more

--- Furniture/Area/Program.cs
+++ Furniture/Area/Program.cs
@@ -9,7 +9,32 @@ namespace Area
     {
         static void Main(string[] args)
         {
+            int Width = 7;
+            int Height = 5;
 
+            Area(Width, Height);
+            Area(Convert.ToDouble(Width), Height);
+            Area(Convert.ToDouble(Width), Convert.ToDouble(Height));
+
+            Console.ReadKey();
+        }
+
+        public static void Area(int Width, int Height)
+        {
+            int Area = Width * Height;
+            Console.WriteLine(Area);
+        }
+
+        public static void Area(double Width, int Height)
+        {
+            double Area = Width * Height;
+            Console.WriteLine(Area);
+        }
+
+        public static void Area(double Width, double Height)
+        {
+            double Area = Width * Height;
+            Console.WriteLine(Area);
         }
     }
 }
---
+++ Furniture/UsingSum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UsingSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+
+            Console.WriteLine("Input how many numbers to add");
+            input = Console.ReadLine();
+        }
+
+        public static void Sum(string input)
+        {
+            string[] sum = new string[10];
+
+            for (int i = 0; i < 11; i++)
+            {
+                if (input = sum.)
+                {
+
+                }
+            }
+        }
+    }
+}
